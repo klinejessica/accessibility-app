404027fc37411cffd3bb0206a12798d8d2d93c46
Jessica Kline <kline.jessica@gmail.com>
2026-08-14 13:18:29 -0400

    Start of app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Quiz } from './quiz/quiz';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: 'quiz', component: Quiz},
+];
