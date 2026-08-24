75eff0fa221dd8472531b34f58b291842abc70ba
Jessica Kline <kline.jessica@gmail.com>
2026-08-24 15:59:09 -0400

    Added payment component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { Quiz } from './quiz/quiz';
+import { Payment } from './payment/payment';
 
 
 export const routes: Routes = [
     {path: 'quiz', component: Quiz},
+    {path: 'payment', component: Payment }
 ];
