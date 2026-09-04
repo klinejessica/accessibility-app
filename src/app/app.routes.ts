c2ccaec37971ed924fdb3ff40ae3dab759d851c9
Jessica Kline <kline.jessica@gmail.com>
2026-09-04 13:14:31 -0400

    added basic inputs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Quiz } from './quiz/quiz';
 import { Payment } from './payment/payment';
+import { Inputs } from './inputs/inputs';
 
 
 export const routes: Routes = [
     {path: 'quiz', component: Quiz},
-    {path: 'payment', component: Payment }
+    {path: 'payment', component: Payment },
+    {path: 'inputs', component: Inputs}
 ];
