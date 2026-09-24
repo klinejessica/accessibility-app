d1a8ab1a4349455084a7a33d4df0b059174332e9
Jessica Kline <kline.jessica@gmail.com>
2026-09-24 19:57:36 -0400

    Created aria component and added to router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Quiz } from './quiz/quiz';
 import { Payment } from './payment/payment';
+import { Aria } from './aria/aria';
 
 
 export const routes: Routes = [
     {path: 'quiz', component: Quiz},
-    {path: 'payment', component: Payment }
+    {path: 'payment', component: Payment },
+    {path: 'aria', component: Aria}
 ];
---
+++ src/app/aria/aria.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aria',
+  imports: [],
+  templateUrl: './aria.html',
+  styleUrl: './aria.css',
+})
+export class Aria {
+
+}
